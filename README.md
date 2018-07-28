2ab63921517fd4d32ea1710d8e2bfa3fbfdd89b5
yiyibao <734262404@qq.com>
2018-07-28 19:34:45 +0800

    初始化

---
+++ README.md
@@ -0,0 +1,21 @@
+# simple-util
+
+小工具
+
+## 简介
+
+## 安装下载
+
+## 快速开始
+
+- 使用文档
+- 二次开发文档
+
+## 交流 & 提问
+
+https://github.com/yiyibao/simple-util/issues
+
+## 关于作者
+
+- 个人主页
+- 收款二维码
---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+
+module.exports = {
+	mode:'production',
+	entry: './src/index.js',
+	output: {
+		path: __dirname,
+		filename: './release/bundle.js'
+	},
+	module:{
+		rules:[
+			{
+				test: /\.js?$/,
+				exclude: /(node_modules)/,
+				loader: 'babel-loader'
+			}
+		]
+	}
+}
